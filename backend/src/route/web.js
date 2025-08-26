a51770c50a712b47f3862af00290b2fcff04ae69
VanHung0103 <nvh11103@gmail.com>
2025-08-26 19:37:32 +0700

    create-project

---
+++ backend/src/route/web.js
@@ -0,0 +1,16 @@
+import express from "express";
+import authController from "../controllers/homeController.js"; // controller cho đăng ký & OTP
+
+let router = express.Router();
+
+const initWebRoutes = (app) => {
+  // API đăng ký (gửi OTP)
+  router.post("/register", authController.register);
+
+  // API xác thực OTP
+  router.post("/verify-otp", authController.verifyOtp);
+
+  return app.use("/", router);
+};
+
+export default initWebRoutes;
