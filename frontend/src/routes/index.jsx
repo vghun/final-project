b5c1aa549ab1ca91314a288b3be87696b6c1c4bf
LELAMLUC <lelamluc1234@gmail.com>
2025-09-09 12:07:52 +0700

    update UI

--- frontend/src/routes/index.jsx
+++ frontend/src/routes/index.jsx
@@ -1,10 +1,14 @@
 import config from "~/config";
 import Home from "~/pages/home";
+import Product from "~/pages/product";
+import ProductDetails from "~/pages/ProductDetails";
 import Profile from "~/pages/profile";
 
 const publicRouter = [
   { path: config.routes.home, component: Home },
   { path: config.routes.profile, component: Profile },
+  { path: config.routes.product, component: Product },
+  { path: config.routes.productDetails, component: ProductDetails },
 ];
 
 const privateRoutes = [];
