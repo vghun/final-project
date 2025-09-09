3d6466b23a16104af6508e3d23c98f2566d7b1da
xdung04 <157205064+xdung04@users.noreply.github.com>
2025-09-09 15:13:49 +0700

    UI trang sản phẩm và phân trang

--- frontend/src/apis/productApi.js
+++ frontend/src/apis/productApi.js
@@ -8,7 +8,9 @@ const productApi = {
   getBestSelling: () => axios.get(`${API_URL}/best-selling`),
   getMostViewed: () => axios.get(`${API_URL}/most-viewed`),
   getTopDiscount: () => axios.get(`${API_URL}/top-discount`),
-  getById: (id) => axios.get(`${API_URL}/${id}`)
+  getById: (id) => axios.get(`${API_URL}/${id}`),
+  getPaged: (page = 1, limit = 15) =>
+  axios.get(`${API_URL}?page=${page}&limit=${limit}`),
 };
 
 export default productApi;
