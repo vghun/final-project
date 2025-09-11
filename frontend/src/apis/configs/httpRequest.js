71150ba4fd15b531ddd69f681d25bef880746abe
vghun <nvh11103@gmail.com>
2025-09-12 01:02:57 +0700

    profile

--- frontend/src/apis/configs/httpRequest.js
+++ frontend/src/apis/configs/httpRequest.js
@@ -1,7 +1,9 @@
 import axios from "axios";
+
 const request = axios.create({
   baseURL: process.env.REACT_APP_BASE_URL,
 });
+
 console.log("Axios baseURL:", request.defaults.baseURL);
 
 export const get = async (path, option = {}) => {
@@ -13,4 +15,16 @@ export const post = async (path, data = {}, option = {}) => {
   const response = await request.post(path, data, option);
   return response.data;
 };
+
+export const put = async (path, data = {}, option = {}) => {
+  const response = await request.put(path, data, option);
+  return response.data;
+};
+
+
+export const del = async (path, option = {}) => {
+  const response = await request.delete(path, option);
+  return response.data;
+};
+
 export default request;
--- frontend/src/components/Popper/UserMenu/index.jsx
+++ frontend/src/components/Popper/UserMenu/index.jsx
@@ -2,6 +2,8 @@ import { useState, useRef } from "react";
 import Tippy from "@tippyjs/react/headless";
 import classNames from "classnames/bind";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useAuth } from "~/context/AuthContext"; 
+import { useNavigate } from "react-router-dom";
 
 import styles from "./UserMenu.module.scss";
 import { Wrapper as PopperWrapper } from "~/components/Popper";
@@ -15,38 +17,54 @@ import {
 const cx = classNames.bind(styles);
 
 function UserMenu({ children }) {
+  const { logout } = useAuth();
   const [visible, setVisible] = useState(false);
   const triggerRef = useRef();
-  const toggleMenu = () => setVisible((prev) => !prev);
+  const navigate = useNavigate();
+
   const hideMenu = () => setVisible(false);
+  const toggleMenu = () => setVisible((prev) => !prev);
+
+  const handleLogout = () => {
+    logout();
+    navigate("/");
+    hideMenu();
+  };
+
+  const handleProfileClick = () => {
+    navigate("/profile");
+    hideMenu();
+  };
+
   const renderResult = (attrs) => (
-    <div className={cx("menu-list")} tabIndex="-1" {...attrs}>
-      <PopperWrapper className={cx("menu-popper")}>
-        <div className={cx("menu-body")}>
-          <div className={cx("body")}>
-            <a className={cx("item")} href="/profile">
-              <FontAwesomeIcon icon={faUserCircle} />
-              <div>Hồ sơ cá nhân</div>
-            </a>
-            <a className={cx("item")} href="#">
-              <FontAwesomeIcon icon={faCalendar} />
-              <div>Lịch hẹn của tôi</div>
-            </a>
-            <a className={cx("item")} href="#">
-              <FontAwesomeIcon icon={faGift} />
-              <div>Điểm tích luỹ</div>
-            </a>
-          </div>
-          <div className={cx("footer")}>
-            <a className={cx("logout")} href="/">
-              <FontAwesomeIcon icon={faRightFromBracket} />
-              <div>Đăng xuất</div>
-            </a>
-          </div>
+  <div className={cx("menu-list")} tabIndex="-1" {...attrs}>
+    <PopperWrapper className={cx("menu-popper")}>
+      <div className={cx("menu-body")}>
+        <div className={cx("body")}>
+          <button className={cx("menu-item")} onClick={handleProfileClick}>
+            <FontAwesomeIcon icon={faUserCircle} />
+            <div>Hồ sơ cá nhân</div>
+          </button>
+          <a className={cx("menu-item")} href="#">
+            <FontAwesomeIcon icon={faCalendar} />
+            <div>Lịch hẹn của tôi</div>
+          </a>
+          <a className={cx("menu-item")} href="#">
+            <FontAwesomeIcon icon={faGift} />
+            <div>Điểm tích luỹ</div>
+          </a>
         </div>
-      </PopperWrapper>
-    </div>
-  );
+        <div className={cx("footer")}>
+          <button className={cx("menu-item")} onClick={handleLogout}>
+            <FontAwesomeIcon icon={faRightFromBracket} />
+            <div>Đăng xuất</div>
+          </button>
+        </div>
+      </div>
+    </PopperWrapper>
+  </div>
+);
+
 
   return (
     <div ref={triggerRef}>
