4e75be1d7f8279e92101a97f8bf629bd46cff069
NguyenVanAn-200704 <nva2004gl81@gmail.com>
2025-09-21 19:03:11 +0700

    commit api login theo role

--- backend/src/middlewares/authMiddleware.js
+++ backend/src/middlewares/authMiddleware.js
@@ -8,9 +8,19 @@ export const authenticate = (req, res, next) => {
   const token = authHeader.split(" ")[1];
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded; // { id, email }
+    // decoded = { id, email, role }
+    req.user = decoded;
     next();
   } catch (err) {
     return res.status(401).json({ message: "Token không hợp lệ." });
   }
+};
+
+export const authorize = (roles = []) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return res.status(403).json({ message: "Không có quyền truy cập." });
+    }
+    next();
+  };
 };
