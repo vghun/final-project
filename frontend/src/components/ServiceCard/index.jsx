44ffd766d411ea825b274f1ac0305bd5794afab6
xdung04 <157205064+xdung04@users.noreply.github.com>
2025-10-09 13:18:28 +0700

    update ui hotService homepage

--- frontend/src/components/ServiceCard/index.jsx
+++ frontend/src/components/ServiceCard/index.jsx
@@ -5,25 +5,24 @@ import styles from "./ServiceCard.module.scss";
 
 const cx = classNames.bind(styles);
 
-function ServiceCard({ id, category, name, description, price, duration }) {
+function ServiceCard({ id, category, name, description, price, duration, image }) {
   const navigate = useNavigate();
 
   return (
     <div className={cx("card")} onClick={() => navigate(`/services/${id}`)}>
-  <div className={cx("content")}>
-    {category && <span className={cx("category")}>{category}</span>}
-    
-    <h4 className={cx("name")}>{name}</h4>
-    <p className={cx("desc")}>{description}</p>
-
-    <div className={cx("footer")}>
-      <span className={cx("price")}>
-        {Number(price).toLocaleString()}đ
-      </span>
-      <span className={cx("duration")}>{duration} phút</span>
+      {image && <img src={image} alt={name} className={cx("cardImage")} />}
+      <div className={cx("content")}>
+        {category && <span className={cx("category")}>{category}</span>}
+        <h4 className={cx("name")}>{name}</h4>
+        <p className={cx("desc")}>{description}</p>
+        <div className={cx("footer")}>
+          <span className={cx("price")}>
+            {Number(price).toLocaleString()}đ
+          </span>
+          <span className={cx("duration")}>{duration} phút</span>
+        </div>
+      </div>
     </div>
-  </div>
-</div>
   );
 }
 
