ffa9c4eb8548d8f73b1e704b0d696f31159a4436
vghun <nvh11103@gmail.com>
2025-10-10 11:48:52 +0700

    update

--- backend/src/models/serviceassignment.js
+++ backend/src/models/serviceassignment.js
@@ -4,18 +4,11 @@ import { Model } from "sequelize";
 export default (sequelize, DataTypes) => {
   class ServiceAssignment extends Model {
     static associate(models) {
-<<<<<<< HEAD
-      ServiceAssignment.belongsTo(models.Branch, { foreignKey: "idBranch" });
-      ServiceAssignment.belongsTo(models.Service, { foreignKey: "idService" });
-    }
-  }
-=======
       this.belongsTo(models.Branch, { foreignKey: "idBranch", as: "branch" });
       this.belongsTo(models.Service, { foreignKey: "idService", as: "service" });
     }
   }
 
->>>>>>> origin/main
   ServiceAssignment.init(
     {
       id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
@@ -24,9 +17,5 @@ export default (sequelize, DataTypes) => {
     },
     { sequelize, modelName: "ServiceAssignment", tableName: "service_assignments" }
   );
-<<<<<<< HEAD
-=======
-
->>>>>>> origin/main
   return ServiceAssignment;
 };
