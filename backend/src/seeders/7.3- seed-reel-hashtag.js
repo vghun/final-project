12e0b003059f0425b3d6aceb68123758ce3579c6
vghun <nvh11103@gmail.com>
2025-10-25 15:02:49 +0700

    update

--- backend/src/seeders/7.3- seed-reel-hashtag.js
+++ backend/src/seeders/7.3- seed-reel-hashtag.js
@@ -2,14 +2,14 @@
 
 export async function up(queryInterface, Sequelize) {
   await queryInterface.bulkInsert("reel_hashtags", [
-    { idReel: 1, idHashtag: 1, createdAt: new Date(), updatedAt: new Date() }, // Fade
-    { idReel: 1, idHashtag: 2, createdAt: new Date(), updatedAt: new Date() }, // BarberStyle
-    { idReel: 2, idHashtag: 3, createdAt: new Date(), updatedAt: new Date() }, // LayerCut
-    { idReel: 2, idHashtag: 4, createdAt: new Date(), updatedAt: new Date() }, // StylePro
-    { idReel: 3, idHashtag: 5, createdAt: new Date(), updatedAt: new Date() }, // KidHaircut
-    { idReel: 3, idHashtag: 6, createdAt: new Date(), updatedAt: new Date() }, // FunBarber
-    { idReel: 4, idHashtag: 7, createdAt: new Date(), updatedAt: new Date() }, // BeardTrim
-    { idReel: 4, idHashtag: 8, createdAt: new Date(), updatedAt: new Date() }, // ClassicMen
+    { idReel: 1, idHashtag: 1, createdAt: new Date() }, // Fade
+    { idReel: 1, idHashtag: 2, createdAt: new Date() }, // BarberStyle
+    { idReel: 2, idHashtag: 3, createdAt: new Date() }, // LayerCut
+    { idReel: 2, idHashtag: 4, createdAt: new Date()}, // StylePro
+    { idReel: 3, idHashtag: 5, createdAt: new Date()}, // KidHaircut
+    { idReel: 3, idHashtag: 6, createdAt: new Date() }, // FunBarber
+    { idReel: 4, idHashtag: 7, createdAt: new Date()}, // BeardTrim
+    { idReel: 4, idHashtag: 8, createdAt: new Date()}, // ClassicMen
   ]);
 }
 
